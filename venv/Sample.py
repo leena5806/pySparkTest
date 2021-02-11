e2301b3f443014896d4ce19ac10ba413000d4384
leena5806 <78906614+leena5806@users.noreply.github.com>
2021-02-11 20:56:12 +0530

    updated

--- venv/Sample.py
+++ venv/Sample.py
@@ -1,5 +1,6 @@
 import pyspark
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType
 
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 data = [("James","Smith","USA","CA"),
@@ -10,3 +11,23 @@ data = [("James","Smith","USA","CA"),
 columns = ["firstname","lastname","country","state"]
 df = spark.createDataFrame(data = data, schema = columns)
 df.show(truncate=False)
+
+data2 = [("James","","Smith","36636","M",3000),
+    ("Michael","Rose","","40288","M",4000),
+    ("Robert","","Williams","42114","M",4000),
+    ("Maria","Anne","Jones","39192","F",4000),
+    ("Jen","Mary","Brown","","F",-1)
+  ]
+
+schema = StructType([ \
+    StructField("firstname",StringType(),True), \
+    StructField("middlename",StringType(),True), \
+    StructField("lastname",StringType(),True), \
+    StructField("id", StringType(), True), \
+    StructField("gender", StringType(), True), \
+    StructField("salary", IntegerType(), True) \
+  ])
+ 
+df1 = spark.createDataFrame(data=data2,schema=schema)
+df1.printSchema()
+df1.show(truncate=False)
