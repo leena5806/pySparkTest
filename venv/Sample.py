30ef2ba2c6e625d0b6603c68e3a31027c509fac5
leena5806 <srileena5806@yahoo.com>
2021-02-11 20:51:41 +0530

    Initial commit

---
+++ venv/Sample.py
@@ -0,0 +1,12 @@
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+data = [("James","Smith","USA","CA"),
+    ("Michael","Rose","USA","NY"),
+    ("Robert","Williams","USA","CA"),
+    ("Maria","Jones","USA","FL")
+  ]
+columns = ["firstname","lastname","country","state"]
+df = spark.createDataFrame(data = data, schema = columns)
+df.show(truncate=False)
